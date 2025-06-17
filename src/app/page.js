e6d48f21b8ff57dafd0506fd454bbfcc19aaa1f5
deepindersingh09 <deepindersingh@edu.sait.ca>
2025-06-17 16:20:38 -0600

    Add Week 5 assignment

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
+'use client';
 import Link from 'next/link';
 
 export default function Page() {
   return (
     <main className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Shopping List</h1>
+      <h1 className="text-3xl font-bold mb-4">CPRG306-Assignments</h1>
       <ul className="list-disc ml-6 space-y-2">
         <li><Link href="/week-2" className="text-blue-500 underline">Go to Week 2</Link></li>
         <li><Link href="/week-3" className="text-blue-500 underline">Go to Week 3</Link></li>
         <li><Link href="/week-4" className="text-blue-500 underline">Go to Week 4</Link></li>
+        <li><Link href="/week-5" className="text-blue-500 underline">Go to Week 5</Link></li> {/* 👈 ADD THIS */}
       </ul>
     </main>
   );
